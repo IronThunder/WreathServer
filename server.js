0d1aaa99c5c1137125f72252b629b88d1b35a4ef
Duncan <duncan.tv8@gmail.com>
2016-08-10 11:47:53 -0400

    again

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ app.get("/customers/subset", function (req, res) {
 });
 
 app.get("/clearall", function(req, res) {
-    db.collection(SCOUTS_COLLECTION).removeMany({}, function(err, doc) {
+    db.collection(CUSTOMERS_COLLECTION).removeMany({}, function(err, doc) {
         if (err) {
             handleError(res, err.message, "Failed to get contact");
         } else {
