8e9a3421d5f92b41373188145d2aa49dcbf34c96
Duncan <duncan.tv8@gmail.com>
2016-08-17 16:52:21 -0400

    again

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.get("/customers/subset", function (req, res) {
 });
 
 app.get("/clearall", function(req, res) {
-    db.collection(SCOUTS_COLLECTION).removeMany({}, function(err, doc) {
+    db.collection(SCOUTS_COLLECTION).removeMany({id: 8}, function(err, doc) {
         if (err) {
             handleError(res, err.message, "Failed to clear all");
         } else {
