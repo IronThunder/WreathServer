c32fc21d2adc2e0ec161a7794866ab2799625dd9
IronThunder <duncan.tv8@gmail.com>
2016-11-16 21:55:34 -0500

    again

--- addScout.sh
+++ addScout.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-curl -X DELETE -H "Content-Type: application/json" -d '{"email":"spl.troop125@gmail.com"}' http://powerful-sea-27631.herokuapp.com/users
-curl -X POST -H "Content-Type: application/json" -d '{"email":"spl.wiltontroop125@gmail.com","name":"Troop 125 SPL","superuser":true}' http://powerful-sea-27631.herokuapp.com/users
+curl -X POST -H "Content-Type: application/json" -d '{"email":"sullivan85@optonline.net.com","name":"Brian Sullivan","superuser":true}' http://powerful-sea-27631.herokuapp.com/users
